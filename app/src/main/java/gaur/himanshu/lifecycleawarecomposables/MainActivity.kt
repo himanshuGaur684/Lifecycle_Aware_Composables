3912815109671b443b9345250e071c88b4e73aa8
Himanshu <himanshugaur684@gmail.com>
2024-12-04 14:02:47 +0530

    Initial commit

---
+++ app/src/main/java/gaur/himanshu/lifecycleawarecomposables/MainActivity.kt
@@ -0,0 +1,34 @@
+package gaur.himanshu.lifecycleawarecomposables
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import gaur.himanshu.lifecycleawarecomposables.ui.theme.LifecycleAwareComposablesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LifecycleAwareComposablesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+}
